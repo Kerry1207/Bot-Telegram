3d03669c7021badbdf7205489e35ce443f05da89
Kerry1207 <45622415+Kerry1207@users.noreply.github.com>
2018-12-06 23:47:02 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,18 +1,6 @@
 <?php
-  
-  $botToken = "bot"."761688361:AAFMrbnvqsFRXK2ra6pT1vVOIVkyGFM2pKs"; $TelegramRawInput = file_get_contents("php://input");
-  $update = json_decode($TelegramRawInput, TRUE);
-  if(!$update)
-{
-  exit;
-}
-  $MessageObj = $update['message'];
-  $chatId = $MessageObj['chat']['id'];
-  $out = sendMsg($botToken,$chatId,"Hello World!");
-  
-function sendMsg($tkn, $cId, $msgTxt)
-{
-  $TelegramUrlSendMessage = "https://api.telegram.org/".$tkn."/sendMessage?chat_id=".$cId."&text=".urlencode($msgTxt);
-  return file_get_contents($TelegramUrlSendMessage);
-}  
+  $token = "761688361:AAFMrbnvqsFRXK2ra6pT1vVOIVkyGFM2pKs";
+  $website = "https://api.telegram.org/bot".$token;
+  $getme = file_get_contents($website."/getme");
+  print_r($getme);
 ?>
