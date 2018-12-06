796f32762a367cc9f0e77c12ba8dfbdacecb78a7
Kerry1207 <45622415+Kerry1207@users.noreply.github.com>
2018-12-06 13:12:09 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,25 +1,18 @@
 <?php
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
-
-if(!$update)
+  
+  $botToken = "bot"."761688361:AAFMrbnvqsFRXK2ra6pT1vVOIVkyGFM2pKs"; $TelegramRawInput = file_get_contents("php://input");
+  $update = json_decode($TelegramRawInput, TRUE);
+  if(!$update)
 {
   exit;
 }
-
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
-
-$text = trim($text);
-$text = strtolower($text);
-
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+  $MessageObj = $update['message'];
+  $chatId = $MessageObj['chat']['id'];
+  $out = sendMsg($botToken,$chatId,"Hello World!");
+  
+function sendMsg($tkn, $cId, $msgTxt)
+{
+  $TelegramUrlSendMessage = "https://api.telegram.org/".$tkn."/sendMessage?chat_id=".$cId."&text=".urlencode($msgTxt);
+  return file_get_contents($TelegramUrlSendMessage);
+}  
+?>
